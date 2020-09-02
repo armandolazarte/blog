6c058602fd07575afd344845653b9680d81ed878
Armando Lazarte <armando_lazarte@hotmail.com>
2020-09-02 13:44:06 -0300

    6. Qué son y cómo utilizar Seeders

--- database/migrations/2020_09_02_124953_create_posts_table.php
+++ database/migrations/2020_09_02_124953_create_posts_table.php
@@ -17,6 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title');
             $table->mediumText('excerpt');
+            $table->text('body');
             $table->timestamp('published_at')->nullable();
             $table->unsignedInteger('category_id');
             $table->timestamps();
